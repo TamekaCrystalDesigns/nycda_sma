c55b648f7110aadf7626f5b66f2e30037de504cd
Tameka Roberts <TamekaCrystalDesigns@gmail.com>
2015-06-18 21:42:46 -0400

    added remaining rails actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,8 +7,54 @@ class StudentsController < ApplicationController
 		@students = Student.all
 
 		def show 
-			binding.pry
 			@student = Student.find_by_id(params[:id])
+		end
+
+		def new
+			@student = Student.new
+		end
+
+		def create
+			binding.pry
+
+			#Initialize a new student object with form values
+			@student = Student.new(student_params)
+
+			if @student.save
+				flash[:notice] = "Student was successfully created!"
+				redirect_to student_path(@student)
+			else
+				render :new
+			end
+		end
+
+		def edit
+			@student = Student.find_by_id(params[:id])	
+		end
+
+		def update
+			@student = Student.find_by_id(params[:id])	
+
+			if @student.update_attributes(student_params)
+				flash[:notice] = "Student was successfully updated"
+				redirect_to_student_path(@student)
+			else
+				render :edit
+			end
+
+		end
+
+		def destroy
+			@student = Student.find_by_id(params[:id])	
+			@student.destroy
+			flash[:notice] = "Student was successfully removed"
+
+		end
+
+		private
+
+		def student_params
+			params.require(:student).permit(:first_name, :last_name, :email, :phone)
 		end
 	end
 end
